bb5a92b6e8961626ddd77c69b35f5fa844fd30a3
devjacjef <dev.jac.jef@proton.me>
2025-07-16 00:37:05 +0100

    Added init file

---
+++ init.el
@@ -0,0 +1,51 @@
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(global-display-line-numbers-mode)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; TODO: Break this into multiple files later...
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode))
+
+;; TODO: Add snippet support
+(use-package lsp-mode
+  :ensure t
+  :hook ((web-mode . lsp))
+  :commands lsp)
+
+(use-package company
+  :ensure t
+  :hook (after-init . global-company-mode))
+
+(use-package web-mode
+  :ensure t
+  :mode
+  (("\\.phtml\\'" . web-mode)
+   ("\\.php\\'" . web-mode)
+   ("\\.tpl\\'" . web-mode)
+   ("\\.[agj]sp\\'" . web-mode)
+   ("\\.as[cp]x\\'" . web-mode)
+   ("\\.erb\\'" . web-mode)
+   ("\\.mustache\\'" . web-mode)
+   ("\\.djhtml\\'" . web-mode)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(company evil lsp-mode web-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
