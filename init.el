34b0cc1737fd04e9c98a5af1fc0a74acb6d5e9e5
Jack Jefferson <jack.jefferson@mtc.co.uk>
2025-11-26 16:34:27 +0000

    refactor: updated emacs.org and added README

--- init.el
+++ init.el
@@ -8,7 +8,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(custom-safe-themes
+   '("d5fd482fcb0fe42e849caba275a01d4925e422963d1cd165565b31d3f4189c87"
+     default))
+ '(package-selected-packages
+   '(cape corfu evil evil-collection go-mode gruvbox-theme kind-icon
+	  lsp-mode magit yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
